e32511c630d6261c427dcf960c48954c1fb520a4
Frank Wunderlich <frank-w@public-files.de>
2019-08-22 12:32:29 +0200

    [build.sh] some improvements (multithreading, check for success) to build

--- build.sh
+++ build.sh
@@ -31,13 +31,19 @@ function upload {
 
 case $1 in
 	"build")
-		make LOCALVERSION="-$ubranch";
-		FILESIZE=$(stat -c%s "u-boot.bin");
-		if [[ $FILESIZE -gt $MAXSIZE ]]; then
-			echo "=============== WARNING ==============="
-			echo "u-boot will overwrite env-storage area!"
-			echo "if you use this u-boot.bin don't use saveenv!"
-		fi;
+		LANG=C
+		CFLAGS=-j$(grep ^processor /proc/cpuinfo  | wc -l)
+		make LOCALVERSION="-$ubranch" ${CFLAGS} 2> build.log;
+		if [[ $? -eq 0 ]];then
+			FILESIZE=$(stat -c%s "u-boot.bin");
+			if [[ $FILESIZE -gt $MAXSIZE ]]; then
+				echo "=============== WARNING ==============="
+				echo "u-boot will overwrite env-storage area!"
+				echo "if you use this u-boot.bin don't use saveenv!"
+			fi;
+		else
+			echo "build failed!"
+		fi
 	;;
 	"config")
 		make menuconfig;

b67287863231a092c68a58c8dfb6431b6cf4dbe0
Frank Wunderlich <frank-w@public-files.de>
2020-08-13 14:26:04 +0200

    ahci: mediatek: update driver after review

--- drivers/ata/mtk_ahci.c
+++ drivers/ata/mtk_ahci.c
@@ -11,17 +11,17 @@
  */
 
 #include <common.h>
-#include <dm.h>
 #include <ahci.h>
-#include <scsi.h>
-#include <sata.h>
-#include <reset.h>
 #include <asm/io.h>
-#include <generic-phy.h>
+#include <dm.h>
 #include <dm/of_access.h>
-#include <syscon.h>
+#include <generic-phy.h>
 #include <linux/err.h>
 #include <regmap.h>
+#include <reset.h>
+#include <sata.h>
+#include <scsi.h>
+#include <syscon.h>
 
 #define SYS_CFG			0x14
 #define SYS_CFG_SATA_MSK	GENMASK(31, 30)
@@ -30,10 +30,9 @@
 struct mtk_ahci_priv {
 	void *base;
 
+	struct ahci_uc_priv ahci_priv;
 	struct regmap *mode;
-	struct reset_ctl axi_rst;
-	struct reset_ctl sw_rst;
-	struct reset_ctl reg_rst;
+	struct reset_ctl_bulk rst_bulk;
 };
 
 static int mtk_ahci_bind(struct udevice *dev)
@@ -47,88 +46,28 @@ static int mtk_ahci_ofdata_to_platdata(struct udevice *dev)
 {
 	struct mtk_ahci_priv *priv = dev_get_priv(dev);
 
-	priv->base = map_physmem(devfdt_get_addr(dev), sizeof(void *),
-				 MAP_NOCACHE);
+	priv->base = devfdt_remap_addr_index(dev, 0);
 
 	return 0;
 }
 
-static int mtk_ahci_platform_resets(struct udevice *dev)
-{
-	struct mtk_ahci_priv *priv = dev_get_priv(dev);
-	int ret;
-
-	ret=reset_get_by_name(dev, "axi",&priv->axi_rst);
-	if (ret) {
-		pr_err("unable to find reset controller 'axi'\n");
-		return ret;
-	}
-
-	ret=reset_get_by_name(dev, "sw",&priv->sw_rst);
-	if (ret) {
-		pr_err("unable to find reset controller 'sw'\n");
-		return ret;
-	}
-
-	ret=reset_get_by_name(dev, "reg",&priv->reg_rst);
-	if (ret) {
-		pr_err("unable to find reset controller 'reg'\n");
-		return ret;
-	}
-
-	ret=reset_assert(&priv->axi_rst);
-	if (ret) {
-		pr_err("unable to assert reset controller 'axi'\n");
-		return ret;
-	}
-
-	ret=reset_assert(&priv->sw_rst);
-	if (ret) {
-		pr_err("unable to assert reset controller 'sw'\n");
-		return ret;
-	}
-
-	ret=reset_assert(&priv->reg_rst);
-	if (ret) {
-		pr_err("unable to assert reset controller 'reg'\n");
-		return ret;
-	}
-
-	ret=reset_deassert(&priv->axi_rst);
-	if (ret) {
-		pr_err("unable to deassert reset controller 'axi'\n");
-		return ret;
-	}
-
-	ret=reset_deassert(&priv->sw_rst);
-	if (ret) {
-		pr_err("unable to deassert reset controller 'sw'\n");
-		return ret;
-	}
-
-	ret=reset_deassert(&priv->reg_rst);
-	if (ret) {
-		pr_err("unable to deassert reset controller 'reg'\n");
-		return ret;
-	}
-	return 0;
-}
-
-static int mtk_ahci_parse_property(struct ahci_uc_priv *hpriv,struct udevice *dev)
+static int mtk_ahci_parse_property(struct ahci_uc_priv *hpriv,
+				   struct udevice *dev)
 {
 	struct mtk_ahci_priv *plat = dev_get_priv(dev);
 	const void *fdt = gd->fdt_blob;
 
 	/* enable SATA function if needed */
-	if (fdt_get_property(fdt, dev_of_offset(dev),"mediatek,phy-mode", NULL)) {
-		plat->mode = syscon_regmap_lookup_by_phandle(
-					dev, "mediatek,phy-mode");
+	if (fdt_get_property(fdt, dev_of_offset(dev),
+			     "mediatek,phy-mode", NULL)) {
+		plat->mode = syscon_regmap_lookup_by_phandle(dev,
+						"mediatek,phy-mode");
 		if (IS_ERR(plat->mode)) {
 			dev_err(dev, "missing phy-mode phandle\n");
 			return PTR_ERR(plat->mode);
 		}
-		regmap_update_bits(plat->mode, SYS_CFG, SYS_CFG_SATA_MSK,
-				   SYS_CFG_SATA_EN);
+		regmap_update_bits(plat->mode, SYS_CFG,
+				   SYS_CFG_SATA_MSK, SYS_CFG_SATA_EN);
 	}
 
 	ofnode_read_u32(dev->node, "ports-implemented", &hpriv->port_map);
@@ -140,21 +79,18 @@ static int mtk_ahci_probe(struct udevice *dev)
 	struct mtk_ahci_priv *priv = dev_get_priv(dev);
 	int ret;
 	struct phy phy;
-	struct ahci_uc_priv *hpriv;
 
-	hpriv = malloc(sizeof(struct ahci_uc_priv));
-	if (!hpriv)
-		return -ENOMEM;
-
-	memset(hpriv, 0, sizeof(struct ahci_uc_priv));
-
-	ret = mtk_ahci_parse_property(hpriv,dev);
+	ret = mtk_ahci_parse_property(&priv->ahci_priv, dev);
 	if (ret)
 		return ret;
 
-	ret = mtk_ahci_platform_resets(dev);
-	if (ret)
-		return ret;
+	ret = reset_get_bulk(dev, &priv->rst_bulk);
+	if (!ret) {
+		reset_assert_bulk(&priv->rst_bulk);
+		reset_deassert_bulk(&priv->rst_bulk);
+	} else {
+		dev_err(dev, "Failed to get reset: %d\n", ret);
+	}
 
 	ret = generic_phy_get_by_name(dev, "sata-phy", &phy);
 	if (ret) {

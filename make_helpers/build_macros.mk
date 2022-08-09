eae8e74e3db98513e0e6dda6d9afd25a4e8eb7c1
Weijie Gao <weijie.gao@mediatek.com>
2022-08-09 17:16:26 +0800

    Build: add xz compression image filter
    Add support to compress images using xz
    
    Signed-off-by: Weijie Gao <weijie.gao@mediatek.com>

--- make_helpers/build_macros.mk
+++ make_helpers/build_macros.mk
@@ -278,6 +278,15 @@ endef
 
 GZIP_SUFFIX := .gz
 
+# XZ
+define XZ_RULE
+$(1): $(2)
+	$(s)echo "  XZ      $$@"
+	$(q)xz -e -k -9 -C crc32 $$< --stdout > $$@
+endef
+
+XZ_SUFFIX := .xz
+
 ################################################################################
 # Auxiliary macros to build TF images from sources
 ################################################################################

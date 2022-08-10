2a66e40f322eb5cc3058ac244f39bc5e6c9339bb
Weijie Gao <weijie.gao@mediatek.com>
2022-08-10 17:47:24 +0800

    mediatek: mt7981: add anti-rollback support
    This patch adds anti-rollback v2 support for mt7981
    
    Signed-off-by: Weijie Gao <weijie.gao@mediatek.com>

--- plat/mediatek/mt7981/bl2/bl2.mk
+++ plat/mediatek/mt7981/bl2/bl2.mk
@@ -50,6 +50,9 @@ include $(MTK_PLAT_SOC)/drivers/dram/dram.mk
 # Trusted board boot
 include $(APSOC_COMMON)/bl2/tbbr.mk
 
+# Anti-rollback
+include $(APSOC_COMMON)/bl2/ar.mk
+
 ifeq ($(BL2_COMPRESS),1)
 BL2_CPPFLAGS		+=	-DUSING_BL2PL
 endif # END OF BL2_COMPRESS

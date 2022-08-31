mtk-openwrt/arm-trusted-firmware

mediatek: mt7622: add initial BL2/BL31 support
Add initial BL2/BL31 support for mt7622.

Signed-off-by: Weijie Gao <weijie.gao@mediatek.com>

plat/mediatek/mt7622/bl2_boot_mmc.c
@@ -1,0 +1,139 @@
+/*
+ * Copyright (c) 2019, MediaTek Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <assert.h>
+#include <drivers/io/io_driver.h>
+#include <drivers/mmc.h>
+#include <drivers/mmc/mtk-sd.h>
+#include <drivers/partition/partition.h>
+#include <common/tbbr/tbbr_img_def.h>
+#include <lib/utils_def.h>
+#include <lib/mmio.h>
+#include <bl2_boot_dev.h>
+#include <pinctrl.h>
+
+struct msdc_gpio_mux_info {
+	const uint32_t *pins;
+	uint32_t count;
+	uint32_t mux;
+};
+
+static const struct msdc_compatible mt7622_msdc0_compat = {
+	.clk_div_bits = 12,
+	.pad_tune0 = true,
+	.async_fifo = true,
+	.data_tune = true,
+	.busy_check = true,
+	.stop_clk_fix = true,
+};
+
+static const struct msdc_compatible mt7622_msdc1_compat = {
+	.clk_div_bits = 12,
+	.pad_tune0 = true,
+	.async_fifo = true,
+	.data_tune = true,
+	.busy_check = true,
+	.stop_clk_fix = true,
+	.r_smpl = true,
+};
+
+static const uint32_t msdc0_pins[] = { 40, 41, 42, 43, 44, 45, 47, 48, 49, 50 };
+static const uint32_t msdc1_pins[] = { 16, 17, 18, 19, 20, 21 };
+
+static const struct msdc_gpio_mux_info msdc0_pinmux = {
+	.pins = msdc0_pins,
+	.count = ARRAY_SIZE(msdc0_pins),
+	.mux = 2,
+};
+
+static const struct msdc_gpio_mux_info msdc1_pinmux = {
+	.pins = msdc1_pins,
+	.count = ARRAY_SIZE(msdc1_pins),
+	.mux = 2,
+};
+
+static const struct mt7622_msdc_conf {
+	uintptr_t base;
+	uint32_t bus_width;
+	enum mmc_device_type type;
+	uint32_t src_clk;
+	const struct msdc_compatible *dev_comp;
+	const struct msdc_gpio_mux_info *pinmux;
+} mt7622_msdc[] = {
+	{
+		.base = MSDC0_BASE,
+		.bus_width = MMC_BUS_WIDTH_8,
+		.type = MMC_IS_EMMC,
+		.src_clk = 48000000,
+		.dev_comp = &mt7622_msdc0_compat,
+		.pinmux = &msdc0_pinmux,
+	}, {
+		.base = MSDC1_BASE,
+		.bus_width = MMC_BUS_WIDTH_4,
+		.type = MMC_IS_SD,
+		.src_clk = 48000000,
+		.dev_comp = &mt7622_msdc1_compat,
+		.pinmux = &msdc1_pinmux,
+	}
+};
+
+static const io_block_dev_spec_t mmc_dev_spec = {
+	.buffer = {
+		.offset = 0x41000000,
+		.length = 0x1000000,
+	},
+	.ops = {
+		.read = mmc_read_blocks,
+	},
+	.block_size = MMC_BLOCK_SIZE,
+};
+
+const io_block_spec_t mtk_boot_dev_gpt_spec = {
+	.offset	= 0 * MMC_BLOCK_SIZE,
+	.length = 34 * MMC_BLOCK_SIZE,
+};
+
+const io_block_spec_t mtk_boot_dev_fip_spec = {
+};
+
+void mtk_boot_dev_setup(const io_dev_connector_t **boot_dev_con,
+			uintptr_t *boot_dev_handle)
+{
+	const struct mt7622_msdc_conf *conf = &mt7622_msdc[MSDC_INDEX];
+	const partition_entry_t *entry;
+	io_block_spec_t *fip_desc = (io_block_spec_t *)&mtk_boot_dev_fip_spec;
+	int i, result;
+
+	for (i = 0; i < conf->pinmux->count; i++)
+		mtk_set_pin_mode(conf->pinmux->pins[i], conf->pinmux->mux);
+
+	mtk_mmc_init(conf->base, conf->dev_comp, conf->src_clk, conf->type,
+		     conf->bus_width);
+
+	result = register_io_dev_block(boot_dev_con);
+	assert(result == 0);
+
+	result = io_dev_open(*boot_dev_con, (uintptr_t)&mmc_dev_spec,
+			     boot_dev_handle);
+	assert(result == 0);
+
+	partition_init(GPT_IMAGE_ID);
+
+	entry = get_partition_entry("fip");
+	if (!entry) {
+		ERROR("Partition 'fip' not found\n");
+		panic();
+	}
+
+	INFO("Located GPT partition 'fip' at 0x%llx, size 0x%llx\n",
+	       entry->start, entry->length);
+
+	fip_desc->offset = entry->start;
+	fip_desc->length = entry->length;
+
+	/* Ignore improbable errors in release builds */
+	(void)result;
+}

plat/mediatek/mt7622/include/platform_def.h
@@ -1,0 +1,145 @@
+/*
+ * Copyright (c) 2019, MediaTek Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef PLATFORM_DEF_H
+#define PLATFORM_DEF_H
+
+#include <common/interrupt_props.h>
+#include <drivers/arm/gic_common.h>
+#include <lib/utils_def.h>
+
+#include "mt7622_def.h"
+
+/*******************************************************************************
+ * Platform binary types for linking
+ ******************************************************************************/
+#define PLATFORM_LINKER_FORMAT		"elf64-littleaarch64"
+#define PLATFORM_LINKER_ARCH		aarch64
+
+/*******************************************************************************
+ * Generic platform constants
+ ******************************************************************************/
+
+/* Size of cacheable stacks */
+#if defined(IMAGE_BL1)
+#define PLATFORM_STACK_SIZE		0x440
+#elif defined(IMAGE_BL2)
+#define PLATFORM_STACK_SIZE		0x1000
+#elif defined(IMAGE_BL31)
+#define PLATFORM_STACK_SIZE		0x800
+#elif defined(IMAGE_BL32)
+#define PLATFORM_STACK_SIZE		0x440
+#endif
+
+#define FIRMWARE_WELCOME_STR		"Booting Trusted Firmware\n"
+
+#define PLATFORM_MAX_AFFLVL		MPIDR_AFFLVL2
+#define PLAT_MAX_PWR_LVL		U(2)
+#define PLAT_MAX_RET_STATE		U(1)
+#define PLAT_MAX_OFF_STATE		U(2)
+#define PLATFORM_SYSTEM_COUNT		1
+#define PLATFORM_CLUSTER_COUNT		1
+#define PLATFORM_CLUSTER0_CORE_COUNT	2
+#define PLATFORM_CLUSTER1_CORE_COUNT	0
+#define PLATFORM_CORE_COUNT		(PLATFORM_CLUSTER1_CORE_COUNT +	\
+					 PLATFORM_CLUSTER0_CORE_COUNT)
+#define PLATFORM_MAX_CPUS_PER_CLUSTER	2
+#define PLATFORM_NUM_AFFS		(PLATFORM_SYSTEM_COUNT +	\
+					 PLATFORM_CLUSTER_COUNT +	\
+					 PLATFORM_CORE_COUNT)
+
+/*******************************************************************************
+ * Platform memory map related constants
+ ******************************************************************************/
+/* TF txet, ro, rw, xlat table, coherent memory ... etc.
+ * Size: release: 128KB, debug: 128KB
+ */
+#define TZRAM_BASE		(0x43000000)
+#if DEBUG
+#define TZRAM_SIZE		(0x20000)
+#else
+#define TZRAM_SIZE		(0x20000)
+#endif
+
+/* Reserved: 64KB */
+#define TZRAM2_BASE		(TZRAM_BASE + TZRAM_SIZE)
+#define TZRAM2_SIZE		(0x10000)
+#define SOC_CHIP_ID		U(0x7622)
+
+/*******************************************************************************
+ * BL2 specific defines.
+ ******************************************************************************/
+/* BL2_BASE is defined in platform.mk */
+#define BL2_LIMIT		(0x240000)
+
+#define MAX_IO_DEVICES		U(3)
+#define MAX_IO_HANDLES		U(4)
+#define MAX_IO_BLOCK_DEVICES	1
+
+/*******************************************************************************
+ * BL31 specific defines.
+ ******************************************************************************/
+#define BL31_BASE		(TZRAM_BASE + 0x1000)
+#define BL31_LIMIT		(TZRAM_BASE + TZRAM_SIZE)
+#define TZRAM2_LIMIT		(TZRAM2_BASE + TZRAM2_SIZE)
+
+#define BL32_BASE		(0x4fd00000)
+#define BL32_LIMIT		(0x300000)
+#define BL32_HEADER_SIZE	(0x1c)
+
+#define BL33_BASE		(0x41e00000)
+
+/*******************************************************************************
+ * Platform specific page table and MMU setup constants
+ ******************************************************************************/
+#define PLAT_PHY_ADDR_SPACE_SIZE	(1ULL << 32)
+#define PLAT_VIRT_ADDR_SPACE_SIZE	(1ULL << 32)
+#define MAX_XLAT_TABLES			4
+#define MAX_MMAP_REGIONS		16
+
+/*******************************************************************************
+ * Declarations and constants to access the mailboxes safely. Each mailbox is
+ * aligned on the biggest cache line size in the platform. This is known only
+ * to the platform as it might have a combination of integrated and external
+ * caches. Such alignment ensures that two maiboxes do not sit on the same cache
+ * line at any cache level. They could belong to different cpus/clusters &
+ * get written while being protected by different locks causing corruption of
+ * a valid mailbox address.
+ ******************************************************************************/
+#define CACHE_WRITEBACK_SHIFT		6
+#define CACHE_WRITEBACK_GRANULE		(1 << CACHE_WRITEBACK_SHIFT)
+
+#define PLAT_ARM_GICD_BASE		BASE_GICD_BASE
+#define PLAT_ARM_GICC_BASE		BASE_GICC_BASE
+
+#define PLAT_ARM_G1S_IRQ_PROPS(grp) \
+	INTR_PROP_DESC(MT_IRQ_SEC_SGI_0, GIC_HIGHEST_SEC_PRIORITY, grp, \
+			GIC_INTR_CFG_EDGE), \
+	INTR_PROP_DESC(MT_IRQ_SEC_SGI_1, GIC_HIGHEST_SEC_PRIORITY, grp, \
+			GIC_INTR_CFG_EDGE), \
+	INTR_PROP_DESC(MT_IRQ_SEC_SGI_2, GIC_HIGHEST_SEC_PRIORITY, grp, \
+			GIC_INTR_CFG_EDGE), \
+	INTR_PROP_DESC(MT_IRQ_SEC_SGI_3, GIC_HIGHEST_SEC_PRIORITY, grp, \
+			GIC_INTR_CFG_EDGE), \
+	INTR_PROP_DESC(MT_IRQ_SEC_SGI_4, GIC_HIGHEST_SEC_PRIORITY, grp, \
+			GIC_INTR_CFG_EDGE), \
+	INTR_PROP_DESC(MT_IRQ_SEC_SGI_5, GIC_HIGHEST_SEC_PRIORITY, grp, \
+			GIC_INTR_CFG_EDGE), \
+	INTR_PROP_DESC(MT_IRQ_SEC_SGI_6, GIC_HIGHEST_SEC_PRIORITY, grp, \
+			GIC_INTR_CFG_EDGE), \
+	INTR_PROP_DESC(MT_IRQ_SEC_SGI_7, GIC_HIGHEST_SEC_PRIORITY, grp, \
+			GIC_INTR_CFG_EDGE)
+
+#define PLAT_ARM_G0_IRQ_PROPS(grp)
+
+/*******************************************************************************
+ * Platform specific IRQ
+   ******************************************************************************/
+#define WDT_IRQ_BIT_ID			(160)
+#define FIQ_SMP_CALL_SGI		(13)
+#define ATF_LOG_IRQ_ID			(281)
+
+#endif /* PLATFORM_DEF_H */

plat/mediatek/mt7622/plat_sip_calls.c
@@ -1,0 +1,98 @@
+/*
+ * Copyright (c) 2019, MediaTek Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <common/debug.h>
+#include <common/runtime_svc.h>
+#include <lib/utils_def.h>
+#include <lib/mmio.h>
+
+#include <mtcmos.h>
+#include <mtk_sip_svc.h>
+#include <plat_sip_calls.h>
+
+/* Authorized secure register list */
+enum {
+	SREG_HDMI_COLOR_EN = 0x14000904
+};
+
+static const uint32_t authorized_sreg[] = {
+	SREG_HDMI_COLOR_EN
+};
+
+#define authorized_sreg_cnt	ARRAY_SIZE(authorized_sreg)
+
+uint64_t mt_sip_set_authorized_sreg(uint32_t sreg, uint32_t val)
+{
+	uint64_t i;
+
+	for (i = 0; i < authorized_sreg_cnt; i++) {
+		if (authorized_sreg[i] == sreg) {
+			mmio_write_32(sreg, val);
+			return MTK_SIP_E_SUCCESS;
+		}
+	}
+
+	return MTK_SIP_E_INVALID_PARAM;
+}
+
+static uint64_t mt_sip_pwr_on_mtcmos(uint32_t val)
+{
+	uint32_t ret;
+
+	ret = mtcmos_non_cpu_ctrl(1, val);
+	if (ret)
+		return MTK_SIP_E_INVALID_PARAM;
+	else
+		return MTK_SIP_E_SUCCESS;
+}
+
+static uint64_t mt_sip_pwr_off_mtcmos(uint32_t val)
+{
+	uint32_t ret;
+
+	ret = mtcmos_non_cpu_ctrl(0, val);
+	if (ret)
+		return MTK_SIP_E_INVALID_PARAM;
+	else
+		return MTK_SIP_E_SUCCESS;
+}
+
+static uint64_t mt_sip_pwr_mtcmos_support(void)
+{
+	return MTK_SIP_E_SUCCESS;
+}
+
+uint64_t mediatek_plat_sip_handler(uint32_t smc_fid,
+				   uint64_t x1,
+				   uint64_t x2,
+				   uint64_t x3,
+				   uint64_t x4,
+				   void *cookie,
+				   void *handle,
+				   uint64_t flags)
+{
+	uint64_t ret;
+
+	switch (smc_fid) {
+	case MTK_SIP_PWR_ON_MTCMOS:
+		ret = mt_sip_pwr_on_mtcmos((uint32_t)x1);
+		SMC_RET1(handle, ret);
+
+	case MTK_SIP_PWR_OFF_MTCMOS:
+		ret = mt_sip_pwr_off_mtcmos((uint32_t)x1);
+		SMC_RET1(handle, ret);
+
+	case MTK_SIP_PWR_MTCMOS_SUPPORT:
+		ret = mt_sip_pwr_mtcmos_support();
+		SMC_RET1(handle, ret);
+
+	default:
+		ERROR("%s: unhandled SMC (0x%x)\n", __func__, smc_fid);
+		break;
+	}
+
+	SMC_RET1(handle, SMC_UNK);
+}

frank-w/u-boot

plat/mediatek/mt7622/bl2_boot_mmc.c
@@ -1,0 +1,139 @@
+/*
+ * Copyright (c) 2019, MediaTek Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <assert.h>
+#include <drivers/io/io_driver.h>
+#include <drivers/mmc.h>
+#include <drivers/mmc/mtk-sd.h>
+#include <drivers/partition/partition.h>
+#include <common/tbbr/tbbr_img_def.h>
+#include <lib/utils_def.h>
+#include <lib/mmio.h>
+#include <bl2_boot_dev.h>
+#include <pinctrl.h>
+
+struct msdc_gpio_mux_info {
+	const uint32_t *pins;
+	uint32_t count;
+	uint32_t mux;
+};
+
+static const struct msdc_compatible mt7622_msdc0_compat = {
+	.clk_div_bits = 12,
+	.pad_tune0 = true,
+	.async_fifo = true,
+	.data_tune = true,
+	.busy_check = true,
+	.stop_clk_fix = true,
+};
+
+static const struct msdc_compatible mt7622_msdc1_compat = {
+	.clk_div_bits = 12,
+	.pad_tune0 = true,
+	.async_fifo = true,
+	.data_tune = true,
+	.busy_check = true,
+	.stop_clk_fix = true,
+	.r_smpl = true,
+};
+
+static const uint32_t msdc0_pins[] = { 40, 41, 42, 43, 44, 45, 47, 48, 49, 50 };
+static const uint32_t msdc1_pins[] = { 16, 17, 18, 19, 20, 21 };
+
+static const struct msdc_gpio_mux_info msdc0_pinmux = {
+	.pins = msdc0_pins,
+	.count = ARRAY_SIZE(msdc0_pins),
+	.mux = 2,
+};
+
+static const struct msdc_gpio_mux_info msdc1_pinmux = {
+	.pins = msdc1_pins,
+	.count = ARRAY_SIZE(msdc1_pins),
+	.mux = 2,
+};
+
+static const struct mt7622_msdc_conf {
+	uintptr_t base;
+	uint32_t bus_width;
+	enum mmc_device_type type;
+	uint32_t src_clk;
+	const struct msdc_compatible *dev_comp;
+	const struct msdc_gpio_mux_info *pinmux;
+} mt7622_msdc[] = {
+	{
+		.base = MSDC0_BASE,
+		.bus_width = MMC_BUS_WIDTH_8,
+		.type = MMC_IS_EMMC,
+		.src_clk = 48000000,
+		.dev_comp = &mt7622_msdc0_compat,
+		.pinmux = &msdc0_pinmux,
+	}, {
+		.base = MSDC1_BASE,
+		.bus_width = MMC_BUS_WIDTH_4,
+		.type = MMC_IS_SD,
+		.src_clk = 48000000,
+		.dev_comp = &mt7622_msdc1_compat,
+		.pinmux = &msdc1_pinmux,
+	}
+};
+
+static const io_block_dev_spec_t mmc_dev_spec = {
+	.buffer = {
+		.offset = 0x41000000,
+		.length = 0x1000000,
+	},
+	.ops = {
+		.read = mmc_read_blocks,
+	},
+	.block_size = MMC_BLOCK_SIZE,
+};
+
+const io_block_spec_t mtk_boot_dev_gpt_spec = {
+	.offset	= 0 * MMC_BLOCK_SIZE,
+	.length = 34 * MMC_BLOCK_SIZE,
+};
+
+const io_block_spec_t mtk_boot_dev_fip_spec = {
+};
+
+void mtk_boot_dev_setup(const io_dev_connector_t **boot_dev_con,
+			uintptr_t *boot_dev_handle)
+{
+	const struct mt7622_msdc_conf *conf = &mt7622_msdc[MSDC_INDEX];
+	const partition_entry_t *entry;
+	io_block_spec_t *fip_desc = (io_block_spec_t *)&mtk_boot_dev_fip_spec;
+	int i, result;
+
+	for (i = 0; i < conf->pinmux->count; i++)
+		mtk_set_pin_mode(conf->pinmux->pins[i], conf->pinmux->mux);
+
+	mtk_mmc_init(conf->base, 0, conf->dev_comp, conf->src_clk, conf->type,
+		     conf->bus_width);
+
+	result = register_io_dev_block(boot_dev_con);
+	assert(result == 0);
+
+	result = io_dev_open(*boot_dev_con, (uintptr_t)&mmc_dev_spec,
+			     boot_dev_handle);
+	assert(result == 0);
+
+	partition_init(GPT_IMAGE_ID);
+
+	entry = get_partition_entry("fip");
+	if (!entry) {
+		ERROR("Partition 'fip' not found\n");
+		panic();
+	}
+
+	INFO("Located GPT partition 'fip' at 0x%lx, size 0x%lx\n",
+	       entry->start, entry->length);
+
+	fip_desc->offset = entry->start;
+	fip_desc->length = entry->length;
+
+	/* Ignore improbable errors in release builds */
+	(void)result;
+}

plat/mediatek/mt7622/include/platform_def.h
@@ -1,0 +1,159 @@
+/*
+ * Copyright (c) 2019, MediaTek Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef PLATFORM_DEF_H
+#define PLATFORM_DEF_H
+
+#include <common/interrupt_props.h>
+#include <drivers/arm/gic_common.h>
+#include <lib/utils_def.h>
+
+#include "mt7622_def.h"
+
+/*******************************************************************************
+ * Platform binary types for linking
+ ******************************************************************************/
+#define PLATFORM_LINKER_FORMAT		"elf64-littleaarch64"
+#define PLATFORM_LINKER_ARCH		aarch64
+
+/*******************************************************************************
+ * Generic platform constants
+ ******************************************************************************/
+
+/* Size of cacheable stacks */
+#if defined(IMAGE_BL1)
+#define PLATFORM_STACK_SIZE		0x440
+#elif defined(IMAGE_BL2)
+#define PLATFORM_STACK_SIZE		0x1000
+#elif defined(IMAGE_BL31)
+#define PLATFORM_STACK_SIZE		0x800
+#elif defined(IMAGE_BL32)
+#define PLATFORM_STACK_SIZE		0x440
+#endif
+
+#define FIRMWARE_WELCOME_STR		"Booting Trusted Firmware\n"
+
+#define PLATFORM_MAX_AFFLVL		MPIDR_AFFLVL2
+#define PLAT_MAX_PWR_LVL		U(2)
+#define PLAT_MAX_RET_STATE		U(1)
+#define PLAT_MAX_OFF_STATE		U(2)
+#define PLATFORM_SYSTEM_COUNT		1
+#define PLATFORM_CLUSTER_COUNT		1
+#define PLATFORM_CLUSTER0_CORE_COUNT	2
+#define PLATFORM_CLUSTER1_CORE_COUNT	0
+#define PLATFORM_CORE_COUNT		(PLATFORM_CLUSTER1_CORE_COUNT +	\
+					 PLATFORM_CLUSTER0_CORE_COUNT)
+#define PLATFORM_MAX_CPUS_PER_CLUSTER	2
+#define PLATFORM_NUM_AFFS		(PLATFORM_SYSTEM_COUNT +	\
+					 PLATFORM_CLUSTER_COUNT +	\
+					 PLATFORM_CORE_COUNT)
+
+/*******************************************************************************
+ * Platform memory map related constants
+ ******************************************************************************/
+/* TF txet, ro, rw, xlat table, coherent memory ... etc.
+ * Size: release: 128KB, debug: 128KB
+ */
+#define TZRAM_BASE		(0x43000000)
+#if DEBUG
+#define TZRAM_SIZE		(0x20000)
+#else
+#define TZRAM_SIZE		(0x20000)
+#endif
+
+/* Reserved: 64KB */
+#define TZRAM2_BASE		(TZRAM_BASE + TZRAM_SIZE)
+#define TZRAM2_SIZE		(0x10000)
+#define SOC_CHIP_ID		U(0x7622)
+
+/*******************************************************************************
+ * BL2 specific defines.
+ ******************************************************************************/
+/* BL2_BASE is defined in platform.mk */
+#define BL2_LIMIT		(0x240000)
+
+#define MAX_IO_DEVICES		U(3)
+#define MAX_IO_HANDLES		U(4)
+#define MAX_IO_BLOCK_DEVICES	1
+
+/*******************************************************************************
+ * BL2PL specific defines.
+ ******************************************************************************/
+#define BL2PL_BASE		(0x100000)
+#define BL2PL_LIMIT		(0x110000)
+
+#define L2_SRAM_BASE		(0x200000)
+#define L2_SRAM_SIZE		(0x40000)
+
+/*******************************************************************************
+ * BL31 specific defines.
+ ******************************************************************************/
+#define BL31_BASE		(TZRAM_BASE + 0x1000)
+#define BL31_LIMIT		(TZRAM_BASE + TZRAM_SIZE)
+
+#define BL32_BASE		(TZRAM2_BASE)
+#define BL32_LIMIT		(TZRAM2_BASE + TZRAM2_SIZE)
+#define BL32_HEADER_SIZE	(0x1c)
+
+#define BL33_BASE		(0x41e00000)
+
+/*******************************************************************************
+ * FIP decompression specific defines.
+ ******************************************************************************/
+#define FIP_DECOMP_TEMP_BASE	(0x42000000)
+#define FIP_DECOMP_TEMP_SIZE	(0x400000)
+
+/*******************************************************************************
+ * Platform specific page table and MMU setup constants
+ ******************************************************************************/
+#define PLAT_PHY_ADDR_SPACE_SIZE	(1ULL << 32)
+#define PLAT_VIRT_ADDR_SPACE_SIZE	(1ULL << 32)
+#define MAX_XLAT_TABLES			4
+#define MAX_MMAP_REGIONS		16
+
+/*******************************************************************************
+ * Declarations and constants to access the mailboxes safely. Each mailbox is
+ * aligned on the biggest cache line size in the platform. This is known only
+ * to the platform as it might have a combination of integrated and external
+ * caches. Such alignment ensures that two maiboxes do not sit on the same cache
+ * line at any cache level. They could belong to different cpus/clusters &
+ * get written while being protected by different locks causing corruption of
+ * a valid mailbox address.
+ ******************************************************************************/
+#define CACHE_WRITEBACK_SHIFT		6
+#define CACHE_WRITEBACK_GRANULE		(1 << CACHE_WRITEBACK_SHIFT)
+
+#define PLAT_ARM_GICD_BASE		BASE_GICD_BASE
+#define PLAT_ARM_GICC_BASE		BASE_GICC_BASE
+
+#define PLAT_ARM_G1S_IRQ_PROPS(grp) \
+	INTR_PROP_DESC(MT_IRQ_SEC_SGI_0, GIC_HIGHEST_SEC_PRIORITY, grp, \
+			GIC_INTR_CFG_EDGE), \
+	INTR_PROP_DESC(MT_IRQ_SEC_SGI_1, GIC_HIGHEST_SEC_PRIORITY, grp, \
+			GIC_INTR_CFG_EDGE), \
+	INTR_PROP_DESC(MT_IRQ_SEC_SGI_2, GIC_HIGHEST_SEC_PRIORITY, grp, \
+			GIC_INTR_CFG_EDGE), \
+	INTR_PROP_DESC(MT_IRQ_SEC_SGI_3, GIC_HIGHEST_SEC_PRIORITY, grp, \
+			GIC_INTR_CFG_EDGE), \
+	INTR_PROP_DESC(MT_IRQ_SEC_SGI_4, GIC_HIGHEST_SEC_PRIORITY, grp, \
+			GIC_INTR_CFG_EDGE), \
+	INTR_PROP_DESC(MT_IRQ_SEC_SGI_5, GIC_HIGHEST_SEC_PRIORITY, grp, \
+			GIC_INTR_CFG_EDGE), \
+	INTR_PROP_DESC(MT_IRQ_SEC_SGI_6, GIC_HIGHEST_SEC_PRIORITY, grp, \
+			GIC_INTR_CFG_EDGE), \
+	INTR_PROP_DESC(MT_IRQ_SEC_SGI_7, GIC_HIGHEST_SEC_PRIORITY, grp, \
+			GIC_INTR_CFG_EDGE)
+
+#define PLAT_ARM_G0_IRQ_PROPS(grp)
+
+/*******************************************************************************
+ * Platform specific IRQ
+   ******************************************************************************/
+#define WDT_IRQ_BIT_ID			(160)
+#define FIQ_SMP_CALL_SGI		(13)
+#define ATF_LOG_IRQ_ID			(281)
+
+#endif /* PLATFORM_DEF_H */

plat/mediatek/mt7622/plat_sip_calls.c
@@ -1,0 +1,98 @@
+/*
+ * Copyright (c) 2019, MediaTek Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <common/debug.h>
+#include <common/runtime_svc.h>
+#include <lib/utils_def.h>
+#include <lib/mmio.h>
+
+#include <mtcmos.h>
+#include <mtk_sip_svc.h>
+#include <plat_sip_calls.h>
+
+/* Authorized secure register list */
+enum {
+	SREG_HDMI_COLOR_EN = 0x14000904
+};
+
+static const uint32_t authorized_sreg[] = {
+	SREG_HDMI_COLOR_EN
+};
+
+#define authorized_sreg_cnt	ARRAY_SIZE(authorized_sreg)
+
+uint64_t mt_sip_set_authorized_sreg(uint32_t sreg, uint32_t val)
+{
+	uint64_t i;
+
+	for (i = 0; i < authorized_sreg_cnt; i++) {
+		if (authorized_sreg[i] == sreg) {
+			mmio_write_32(sreg, val);
+			return MTK_SIP_E_SUCCESS;
+		}
+	}
+
+	return MTK_SIP_E_INVALID_PARAM;
+}
+
+static uint64_t mt_sip_pwr_on_mtcmos(uint32_t val)
+{
+	uint32_t ret;
+
+	ret = mtcmos_non_cpu_ctrl(1, val);
+	if (ret)
+		return MTK_SIP_E_INVALID_PARAM;
+	else
+		return MTK_SIP_E_SUCCESS;
+}
+
+static uint64_t mt_sip_pwr_off_mtcmos(uint32_t val)
+{
+	uint32_t ret;
+
+	ret = mtcmos_non_cpu_ctrl(0, val);
+	if (ret)
+		return MTK_SIP_E_INVALID_PARAM;
+	else
+		return MTK_SIP_E_SUCCESS;
+}
+
+static uint64_t mt_sip_pwr_mtcmos_support(void)
+{
+	return MTK_SIP_E_SUCCESS;
+}
+
+uintptr_t mediatek_plat_sip_handler(uint32_t smc_fid,
+				    u_register_t x1,
+				    u_register_t x2,
+				    u_register_t x3,
+				    u_register_t x4,
+				    void *cookie,
+				    void *handle,
+				    u_register_t flags)
+{
+	uint64_t ret;
+
+	switch (smc_fid) {
+	case MTK_SIP_PWR_ON_MTCMOS:
+		ret = mt_sip_pwr_on_mtcmos((uint32_t)x1);
+		SMC_RET1(handle, ret);
+
+	case MTK_SIP_PWR_OFF_MTCMOS:
+		ret = mt_sip_pwr_off_mtcmos((uint32_t)x1);
+		SMC_RET1(handle, ret);
+
+	case MTK_SIP_PWR_MTCMOS_SUPPORT:
+		ret = mt_sip_pwr_mtcmos_support();
+		SMC_RET1(handle, ret);
+
+	default:
+		ERROR("%s: unhandled SMC (0x%x)\n", __func__, smc_fid);
+		break;
+	}
+
+	SMC_RET1(handle, SMC_UNK);
+}
mtk-openwrt/arm-trusted-firmware

mediatek: mt7629: add initial BL2/BL32 support
Add initial BL2/BL32 support for mt7629.

Signed-off-by: Weijie Gao <weijie.gao@mediatek.com>

plat/mediatek/mt7629/include/mt7629_def.h
@@ -1,0 +1,95 @@
+/*
+ * Copyright (c) 2020, MediaTek Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef MT7629_DEF_H
+#define MT7629_DEF_H
+
+#if RESET_TO_BL31
+#error "MT7629 is incompatible with RESET_TO_BL31!"
+#endif
+
+#define XTAL_CLK			40000000
+#define ARCH_TIMER_CLK			20000000
+
+#define MT7629_PRIMARY_CPU		0x0
+
+/* Register base address */
+
+#define INFRACFG_AO_BASE		0x10000000
+#define PERICFG_BASE			0x10002000
+#define SRAMROM_SEC_BASE		0x10000800
+#define SRAMROM_SEC_CTRL		(SRAMROM_SEC_BASE + 0x4)
+#define SRAMROM_SEC_ADDR		(SRAMROM_SEC_BASE + 0x8)
+
+#define SPM_BASE			0x10006000
+#define MCUCFG_BASE			0x10200000
+#define EMI_BASE			0x10203000
+#define EFUSE_BASE			0x10206000
+#define APMIXED_BASE			0x10209000
+#define TRNG_BASE			0x1020f000
+#define TOPCKGEN_BASE			0x10210000
+#define RGU_BASE			0x10212000
+#define DDRPHY_BASE			0x10213000
+#define DRAMC_AO_BASE			0x10214000
+#define DRAMC_NAO_BASE			0x1020e000
+#define GPIO_BASE			0x10210000
+#define NFI_BASE			0x1100d000
+#define NFIECC_BASE			0x1100e000
+
+/* Watchdog */
+#define MTK_WDT_BASE			RGU_BASE
+#define MTK_WDT_SWRST			(MTK_WDT_BASE + 0x14)
+#define MTK_WDT_MODE_EXTEN		0x0004
+#define MTK_WDT_MODE_IRQ		0x0008
+#define MTK_WDT_MODE_DUAL_MODE		0x0040
+#define MTK_WDT_SWRST_KEY		0x1209
+#define MTK_WDT_MODE_KEY		0x22000000
+#define MTK_WDT_MODE_ENABLE		0x0001
+
+/* GIC-400 & Interrupt handling */
+#define BASE_GIC_BASE			0x10310000
+#define BASE_GICD_BASE			0x10310000
+#define BASE_GICC_BASE			0x10320000
+#define BASE_GICR_BASE			0	/* no GICR in GIC-400 */
+#define BASE_GICH_BASE			0x10340000
+#define BASE_GICV_BASE			0x10360000
+#define GIC_PRIVATE_SIGNALS     	32
+
+#define INT_POL_CTL0			0x10200620
+#define INT_POL_SECCTL0			0x10200a00
+#define INT_POL_SECCTL_NUM		8
+
+/* CCI-400 */
+#define CCI_BASE			0x10390000
+#define CCI_CLUSTER0_SL_IFACE_IX	4
+#define CCI_CLUSTER1_SL_IFACE_IX	3
+#define CCI_SEC_ACCESS			0x8
+
+/* FIXME: Aggregate of all devices in the first GB */
+#define MTK_DEV_RNG0_BASE		0x10000000
+#define MTK_DEV_RNG0_SIZE		0x400000
+#define MTK_DEV_RNG1_BASE		0x11000000
+#define MTK_DEV_RNG1_SIZE		0x1000000
+
+/* UART */
+#define UART0_BASE			0x11002000
+#define UART_BAUDRATE			115200
+
+/* System counter frequency */
+#define SYS_COUNTER_FREQ_IN_TICKS	12500000
+#define SYS_COUNTER_FREQ_IN_TICKS2	20000000
+
+/* FIQ platform related define */
+#define MT_IRQ_SEC_SGI_0		8
+#define MT_IRQ_SEC_SGI_1		9
+#define MT_IRQ_SEC_SGI_2		10
+#define MT_IRQ_SEC_SGI_3		11
+#define MT_IRQ_SEC_SGI_4		12
+#define MT_IRQ_SEC_SGI_5		13
+#define MT_IRQ_SEC_SGI_6		14
+#define MT_IRQ_SEC_SGI_7		15
+
+#endif /* MT7629_DEF_H */

plat/mediatek/mt7629/sp_min/sp_min_setup.c
@@ -1,0 +1,133 @@
+/*
+ * Copyright (c) 2020, MediaTek Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <assert.h>
+
+#include <platform_def.h>
+
+#include <arch_helpers.h>
+#include <common/bl_common.h>
+#include <common/debug.h>
+#include <common/desc_image_load.h>
+#include <drivers/console.h>
+#include <drivers/generic_delay_timer.h>
+#include <hsuart.h>
+#include <lib/mmio.h>
+#include <plat_private.h>
+#include <plat/common/platform.h>
+
+#include <mtk_plat_common.h>
+#include <cpuxgpt.h>
+#include <spmc.h>
+/******************************************************************************
+ * Placeholder variables for copying the arguments that have been passed to
+ * BL32 from BL2.
+ ******************************************************************************/
+static entry_point_info_t bl33_image_ep_info;
+
+void plat_mt_gic_driver_init(void);
+void plat_mt_gic_init(void);
+
+static void platform_setup_sram(void)
+{
+	/* protect BL32 memory from non-secure read/write access */
+	mmio_write_32(SRAMROM_SEC_ADDR,
+		      (uint32_t)(BL32_END + 0x3ff) & 0x3fc00);
+	mmio_write_32(SRAMROM_SEC_CTRL, 0x10000ff9);
+}
+
+/*******************************************************************************
+ * Return a pointer to the 'entry_point_info' structure of the next image for
+ * the security state specified. BL33 corresponds to the non-secure image type.
+ * A NULL pointer is returned if the image does not exist.
+ ******************************************************************************/
+entry_point_info_t *sp_min_plat_get_bl33_ep_info(void)
+{
+	entry_point_info_t *next_image_info;
+
+	next_image_info = &bl33_image_ep_info;
+
+	if (next_image_info->pc == 0U)
+		return NULL;
+
+	return next_image_info;
+}
+
+/*******************************************************************************
+ * Perform any BL32 specific platform actions.
+ ******************************************************************************/
+void sp_min_early_platform_setup2(u_register_t arg0, u_register_t arg1,
+				  u_register_t arg2, u_register_t arg3)
+{
+	static console_t console;
+
+	console_hsuart_register(UART0_BASE, plat_uart_clock(), UART_BAUDRATE,
+				true, &console);
+
+	VERBOSE("sp_min_setup\n");
+
+	bl_params_t *params_from_bl2 = (bl_params_t *)arg0;
+
+	/* Imprecise aborts can be masked in NonSecure */
+	write_scr(read_scr() | SCR_AW_BIT);
+
+	assert(params_from_bl2 != NULL);
+	assert(params_from_bl2->h.type == PARAM_BL_PARAMS);
+	assert(params_from_bl2->h.version >= VERSION_2);
+
+	bl_params_node_t *bl_params = params_from_bl2->head;
+
+	/*
+	 * Copy BL33 entry point information.
+	 * They are stored in Secure RAM, in BL2's address space.
+	 */
+	while (bl_params != NULL) {
+		if (bl_params->image_id == BL33_IMAGE_ID) {
+			bl33_image_ep_info = *bl_params->ep_info;
+			break;
+		}
+
+		bl_params = bl_params->next_params_info;
+	}
+}
+
+/*******************************************************************************
+ * Perform any sp_min platform setup code
+ ******************************************************************************/
+void sp_min_platform_setup(void)
+{
+	generic_delay_timer_init();
+	platform_setup_sram();
+
+	/* Initialize the gic cpu and distributor interfaces */
+	plat_mt_gic_driver_init();
+	plat_mt_gic_init();
+
+	/* Power off CPU1 */
+	mtk_spm_cpu1_power_off();
+}
+
+/*******************************************************************************
+ * Perform the very early platform specific architectural setup here. At the
+ * moment this is only intializes the mmu in a quick and dirty way.
+ ******************************************************************************/
+void sp_min_plat_arch_setup(void)
+{
+	plat_cci_init();
+	plat_cci_enable();
+
+	plat_configure_mmu_svc_mon(BL_CODE_BASE,
+				   BL_COHERENT_RAM_END - BL_CODE_BASE,
+				   BL_CODE_BASE,
+				   BL_CODE_END,
+				   BL_COHERENT_RAM_BASE,
+				   BL_COHERENT_RAM_END);
+}
+
+void sp_min_plat_fiq_handler(uint32_t id)
+{
+	VERBOSE("[sp_min] interrupt #%d\n", id);
+}

frank-w/u-boot

plat/mediatek/mt7629/include/mt7629_def.h
@@ -1,0 +1,92 @@
+/*
+ * Copyright (c) 2020, MediaTek Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef MT7629_DEF_H
+#define MT7629_DEF_H
+
+#if RESET_TO_BL31
+#error "MT7629 is incompatible with RESET_TO_BL31!"
+#endif
+
+#define XTAL_CLK			40000000
+#define ARCH_TIMER_CLK			20000000
+
+#define MT7629_PRIMARY_CPU		0x0
+
+/* Register base address */
+
+#define INFRACFG_AO_BASE		0x10000000
+#define PERICFG_BASE			0x10002000
+#define SRAMROM_SEC_BASE		0x10000800
+#define SRAMROM_SEC_CTRL		(SRAMROM_SEC_BASE + 0x4)
+#define SRAMROM_SEC_ADDR		(SRAMROM_SEC_BASE + 0x8)
+
+#define SPM_BASE			0x10006000
+#define MCUCFG_BASE			0x10200000
+#define EMI_BASE			0x10203000
+#define EFUSE_BASE			0x10206000
+#define APMIXED_BASE			0x10209000
+#define TRNG_BASE			0x1020f000
+#define TOPCKGEN_BASE			0x10210000
+#define RGU_BASE			0x10212000
+#define DDRPHY_BASE			0x10213000
+#define DRAMC_AO_BASE			0x10214000
+#define DRAMC_NAO_BASE			0x1020e000
+#define GPIO_BASE			0x10210000
+#define NFI_BASE			0x1100d000
+#define NFIECC_BASE			0x1100e000
+
+/* Watchdog */
+#define WDT_BASE			RGU_BASE
+
+/* GIC-400 & Interrupt handling */
+#define BASE_GIC_BASE			0x10310000
+#define BASE_GICD_BASE			0x10310000
+#define BASE_GICC_BASE			0x10320000
+#define BASE_GICR_BASE			0	/* no GICR in GIC-400 */
+#define BASE_GICH_BASE			0x10340000
+#define BASE_GICV_BASE			0x10360000
+#define GIC_PRIVATE_SIGNALS     	32
+
+#define INT_POL_CTL0			0x10200620
+#define INT_POL_SECCTL0			0x10200a00
+#define INT_POL_SECCTL_NUM		8
+
+/* CCI-400 */
+#define CCI_BASE			0x10390000
+#define CCI_CLUSTER0_SL_IFACE_IX	4
+#define CCI_CLUSTER1_SL_IFACE_IX	3
+#define CCI_SEC_ACCESS			0x8
+
+/* FIXME: Aggregate of all devices in the first GB */
+#define MTK_DEV_RNG0_BASE		0x10000000
+#define MTK_DEV_RNG0_SIZE		0x400000
+#define MTK_DEV_RNG1_BASE		0x11000000
+#define MTK_DEV_RNG1_SIZE		0x1000000
+
+/* UART */
+#define UART0_BASE			0x11002000
+#define UART_BASE			UART0_BASE
+#define UART_BAUDRATE			115200
+
+/* System counter frequency */
+#define SYS_COUNTER_FREQ_IN_TICKS	12500000
+#define SYS_COUNTER_FREQ_IN_TICKS2	20000000
+
+/* FIQ platform related define */
+#define MT_IRQ_SEC_SGI_0		8
+#define MT_IRQ_SEC_SGI_1		9
+#define MT_IRQ_SEC_SGI_2		10
+#define MT_IRQ_SEC_SGI_3		11
+#define MT_IRQ_SEC_SGI_4		12
+#define MT_IRQ_SEC_SGI_5		13
+#define MT_IRQ_SEC_SGI_6		14
+#define MT_IRQ_SEC_SGI_7		15
+
+/* Define maximum page size for NAND devices */
+#define PLATFORM_MTD_MAX_PAGE_SIZE	0x1000
+
+#endif /* MT7629_DEF_H */

plat/mediatek/mt7629/sp_min/sp_min_setup.c
@@ -1,0 +1,154 @@
+/*
+ * Copyright (c) 2020, MediaTek Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <assert.h>
+
+#include <platform_def.h>
+
+#include <arch_helpers.h>
+#include <common/bl_common.h>
+#include <common/debug.h>
+#include <common/desc_image_load.h>
+#include <drivers/console.h>
+#include <drivers/generic_delay_timer.h>
+#include <hsuart.h>
+#include <lib/mmio.h>
+#include <plat_private.h>
+#include <plat/common/platform.h>
+
+#include <mtk_plat_common.h>
+#include <cpuxgpt.h>
+#include <spmc.h>
+/******************************************************************************
+ * Placeholder variables for copying the arguments that have been passed to
+ * BL32 from BL2.
+ ******************************************************************************/
+static entry_point_info_t bl33_image_ep_info;
+
+void plat_mt_gic_driver_init(void);
+void plat_mt_gic_init(void);
+
+static void platform_setup_sram(void)
+{
+	/* protect BL32 memory from non-secure read/write access */
+	mmio_write_32(SRAMROM_SEC_ADDR,
+		      (uint32_t)(BL32_END + 0x3ff) & 0x3fc00);
+	mmio_write_32(SRAMROM_SEC_CTRL, 0x10000ff9);
+}
+
+/*******************************************************************************
+ * Return a pointer to the 'entry_point_info' structure of the next image for
+ * the security state specified. BL33 corresponds to the non-secure image type.
+ * A NULL pointer is returned if the image does not exist.
+ ******************************************************************************/
+entry_point_info_t *sp_min_plat_get_bl33_ep_info(void)
+{
+	entry_point_info_t *next_image_info;
+
+	next_image_info = &bl33_image_ep_info;
+
+	if (next_image_info->pc == 0U)
+		return NULL;
+
+	return next_image_info;
+}
+
+/*******************************************************************************
+ * Perform any BL32 specific platform actions.
+ ******************************************************************************/
+void sp_min_early_platform_setup2(u_register_t arg0, u_register_t arg1,
+				  u_register_t arg2, u_register_t arg3)
+{
+	static console_t console;
+
+	console_hsuart_register(UART_BASE, plat_uart_clock(), UART_BAUDRATE,
+				true, &console);
+
+	VERBOSE("sp_min_setup\n");
+
+	/*
+	 * setup mpu to protect TZRAM (0x43000000 ~ 0x43200000)
+	 * disallow non-secure read and write
+	 */
+	mmio_write_32(0x10203160, 0x03000320);
+	mmio_write_32(0x102031A0, 0x00008249);
+
+	/*
+	 * setup devapc to protect eFuse
+	 * disallow non-secure write
+	 */
+	mmio_write_32(0x10007f00, 0x80000001);
+	mmio_write_32(0x10207f00, 0x80000001);
+	mmio_write_32(0x10007004, 0x00000080);
+
+	/*
+	 * setup devapc to protect devapc
+	 * disallow non-secure read and write
+	 */
+	mmio_write_32(0x10007000, 0x00004000);
+
+	bl_params_t *params_from_bl2 = (bl_params_t *)arg0;
+
+	/* Imprecise aborts can be masked in NonSecure */
+	write_scr(read_scr() | SCR_AW_BIT);
+
+	assert(params_from_bl2 != NULL);
+	assert(params_from_bl2->h.type == PARAM_BL_PARAMS);
+	assert(params_from_bl2->h.version >= VERSION_2);
+
+	bl_params_node_t *bl_params = params_from_bl2->head;
+
+	/*
+	 * Copy BL33 entry point information.
+	 * They are stored in Secure RAM, in BL2's address space.
+	 */
+	while (bl_params != NULL) {
+		if (bl_params->image_id == BL33_IMAGE_ID) {
+			bl33_image_ep_info = *bl_params->ep_info;
+			break;
+		}
+
+		bl_params = bl_params->next_params_info;
+	}
+}
+
+/*******************************************************************************
+ * Perform any sp_min platform setup code
+ ******************************************************************************/
+void sp_min_platform_setup(void)
+{
+	generic_delay_timer_init();
+	platform_setup_sram();
+
+	/* Initialize the gic cpu and distributor interfaces */
+	plat_mt_gic_driver_init();
+	plat_mt_gic_init();
+
+	/* Power off CPU1 */
+	mtk_spm_cpu1_power_off();
+}
+
+/*******************************************************************************
+ * Perform the very early platform specific architectural setup here. At the
+ * moment this is only intializes the mmu in a quick and dirty way.
+ ******************************************************************************/
+void sp_min_plat_arch_setup(void)
+{
+	plat_cci_init();
+	plat_cci_enable();
+
+	plat_configure_mmu_svc_mon(BL_CODE_BASE,
+				   BL_COHERENT_RAM_END - BL_CODE_BASE,
+				   BL_CODE_BASE,
+				   BL_CODE_END,
+				   BL_COHERENT_RAM_BASE,
+				   BL_COHERENT_RAM_END);
+}
+
+void sp_min_plat_fiq_handler(uint32_t id)
+{
+	VERBOSE("[sp_min] interrupt #%d\n", id);
+}
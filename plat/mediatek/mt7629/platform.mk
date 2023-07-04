86f174a2f54e0a43fb07a045b666e86052e307e2
Weijie Gao <weijie.gao@mediatek.com>
2023-07-04 16:50:33 +0800

    mediatek: mt7629: add build option dependency rules
    Add build option dependency rules to make sure option changes take effect
    on .o files immediately to avoid cleaning current build.
    
    Signed-off-by: Weijie Gao <weijie.gao@mediatek.com>

--- plat/mediatek/mt7629/platform.mk
+++ plat/mediatek/mt7629/platform.mk
@@ -52,3 +52,15 @@ include $(MTK_PLAT_SOC)/drivers/efuse/efuse.mk
 
 include $(MTK_PLAT)/apsoc_common/bl2/tbbr_post.mk
 include $(MTK_PLAT)/apsoc_common/bl2/bl2_image_post.mk
+
+# Make sure make command parameter takes effect on .o files immediately
+include make_helpers/dep.mk
+
+$(call GEN_DEP_RULES,bl2,bl2_boot_ram bl2_boot_nand_nmbm mtk_efuse bl2_plat_init)
+$(call MAKE_DEP,bl2,bl2_plat_init,BL2_COMPRESS)
+$(call MAKE_DEP,bl2,bl2_boot_ram,RAM_BOOT_DEBUGGER_HOOK RAM_BOOT_UART_DL)
+$(call MAKE_DEP,bl2,bl2_boot_nand_nmbm,NMBM_MAX_RATIO NMBM_MAX_RESERVED_BLOCKS NMBM_DEFAULT_LOG_LEVEL)
+$(call MAKE_DEP,bl2,mtk_efuse,TRUSTED_BOARD_BOOT)
+
+$(call GEN_DEP_RULES,bl32,mtk_efuse)
+$(call MAKE_DEP,bl32,mtk_efuse,TRUSTED_BOARD_BOOT)

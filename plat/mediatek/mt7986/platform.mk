56ed1505c0e5e4345fc493d93935dd082d5ecf77
Weijie Gao <weijie.gao@mediatek.com>
2023-07-04 16:51:33 +0800

    mediatek: mt7986: add build option dependency rules
    Add build option dependency rules to make sure option changes take effect
    on .o files immediately to avoid cleaning current build.
    
    Signed-off-by: Weijie Gao <weijie.gao@mediatek.com>

--- plat/mediatek/mt7986/platform.mk
+++ plat/mediatek/mt7986/platform.mk
@@ -45,3 +45,19 @@ include $(MTK_PLAT_SOC)/drivers/efuse/efuse.mk
 include $(MTK_PLAT)/apsoc_common/bl2/tbbr_post.mk
 include $(MTK_PLAT)/apsoc_common/bl2/ar_post.mk
 include $(MTK_PLAT)/apsoc_common/bl2/bl2_image_post.mk
+
+# Make sure make command parameter reflects on .o files immediately
+include make_helpers/dep.mk
+
+$(call GEN_DEP_RULES,bl2,emicfg bl2_boot_ram bl2_boot_nand_nmbm bl2_dev_mmc mtk_efuse bl2_plat_init bl2_plat_setup mt7986_gpio)
+$(call MAKE_DEP,bl2,emicfg,DRAM_USE_DDR4 DRAM_SIZE_LIMIT DRAM_DEBUG_LOG)
+$(call MAKE_DEP,bl2,bl2_plat_init,BL2_COMPRESS I2C_SUPPORT)
+$(call MAKE_DEP,bl2,bl2_plat_setup,BOOT_DEVICE TRUSTED_BOARD_BOOT)
+$(call MAKE_DEP,bl2,bl2_dev_mmc,BOOT_DEVICE)
+$(call MAKE_DEP,bl2,bl2_boot_ram,RAM_BOOT_DEBUGGER_HOOK RAM_BOOT_UART_DL)
+$(call MAKE_DEP,bl2,bl2_boot_nand_nmbm,NMBM_MAX_RATIO NMBM_MAX_RESERVED_BLOCKS NMBM_DEFAULT_LOG_LEVEL)
+$(call MAKE_DEP,bl2,mtk_efuse,ANTI_ROLLBACK TRUSTED_BOARD_BOOT)
+$(call MAKE_DEP,bl2,mt7986_gpio,ENABLE_JTAG)
+
+$(call GEN_DEP_RULES,bl31,mtk_efuse)
+$(call MAKE_DEP,bl31,mtk_efuse,ANTI_ROLLBACK TRUSTED_BOARD_BOOT)

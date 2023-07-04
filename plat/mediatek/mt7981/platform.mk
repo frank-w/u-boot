6ecc0f2ea24d948ec9cedaecada5294ded47644a
Weijie Gao <weijie.gao@mediatek.com>
2023-07-04 16:53:12 +0800

    mediatek: mt7981: add build option dependency rules
    Add build option dependency rules to make sure option changes take effect
    on .o files immediately to avoid cleaning current build.
    
    Signed-off-by: Weijie Gao <weijie.gao@mediatek.com>

--- plat/mediatek/mt7981/platform.mk
+++ plat/mediatek/mt7981/platform.mk
@@ -52,3 +52,19 @@ OPTEE_TZRAM_SIZE := 0x500000
 endif
 endif
 CPPFLAGS += -DOPTEE_TZRAM_SIZE=$(OPTEE_TZRAM_SIZE)
+
+# Make sure make command parameter reflects on .o files immediately
+include make_helpers/dep.mk
+
+$(call GEN_DEP_RULES,bl2,emicfg bl2_boot_ram bl2_boot_nand_nmbm bl2_dev_mmc bl2_plat_init bl2_plat_setup mt7981_gpio dtb)
+$(call MAKE_DEP,bl2,emicfg,DRAM_USE_DDR4 DRAM_SIZE_LIMIT DRAM_DEBUG_LOG DDR3_FREQ_2133 DDR3_FREQ_1866 BOARD_QFN BOARD_BGA)
+$(call MAKE_DEP,bl2,bl2_plat_init,BL2_COMPRESS)
+$(call MAKE_DEP,bl2,bl2_plat_setup,BOOT_DEVICE TRUSTED_BOARD_BOOT)
+$(call MAKE_DEP,bl2,bl2_dev_mmc,BOOT_DEVICE)
+$(call MAKE_DEP,bl2,bl2_boot_ram,RAM_BOOT_DEBUGGER_HOOK RAM_BOOT_UART_DL)
+$(call MAKE_DEP,bl2,bl2_boot_nand_nmbm,NMBM_MAX_RATIO NMBM_MAX_RESERVED_BLOCKS NMBM_DEFAULT_LOG_LEVEL)
+$(call MAKE_DEP,bl2,mt7981_gpio,ENABLE_JTAG)
+$(call MAKE_DEP,bl2,dtb,BOOT_DEVICE)
+
+$(call GEN_DEP_RULES,bl31,bl31_plat_setup)
+$(call MAKE_DEP,bl31,bl31_plat_setup,ANTI_ROLLBACK TRUSTED_BOARD_BOOT)

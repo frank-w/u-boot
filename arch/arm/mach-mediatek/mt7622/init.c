ded2c508b148d2e2cf9afb3bac27fb2c74bc90f3
Frank Wunderlich <frank-w@public-files.de>
2024-05-19 13:48:20 +0200

    mt7622: try to add early uart support for of_upstream conversion

--- arch/arm/mach-mediatek/mt7622/init.c
+++ arch/arm/mach-mediatek/mt7622/init.c
@@ -11,6 +11,7 @@
 #include <asm/global_data.h>
 #include <asm/u-boot.h>
 #include <linux/sizes.h>
+#include <asm/io.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -36,6 +37,54 @@ int dram_init(void)
 void reset_cpu(void)
 {
 	psci_system_reset();
+}
+
+#define UART_BASE						0x11002000
+#define UART_BAUD						115200
+
+// Real XTAL (MT7622/MT7986/MT7988)
+#define UART_CLK						40000000
+
+#define UART_RBR						(0x0)
+#define UART_THR						(0x0)
+#define UART_DLL						(0x0)
+#define UART_DLH						(0x4)
+#define UART_IER						(0x4)
+#define UART_LCR						(0xc)
+#define UART_LSR						(0x14)
+#define UART_HIGHSPEED					(0x24)
+#define UART_SAMPLE_COUNT				(0x28)
+#define UART_SAMPLE_POINT				(0x2c)
+
+#define UART_LSR_DR						(0x01)	/* Data ready */
+#define UART_LSR_THRE					(0x20)	/* Xmit holding register empty */
+
+#define UART_WRITE_REG(offset, val)		setbits_32(UART_BASE + (offset), val)
+#define UART_READ_REG(offset)			readl(UART_BASE + (offset))
+
+#define QUOT_VAL						((UART_CLK / (0xff * UART_BAUD)) + 1)
+#define SAMPLE_COUNT_VAL				((UART_CLK / (UART_BAUD * QUOT_VAL)) - 1)
+#define SAMPLE_POINT_VAL				((SAMPLE_COUNT_VAL - 1) / 2)
+
+
+void board_debug_uart_init(void)
+{
+	int tmp;
+
+	UART_WRITE_REG(UART_HIGHSPEED, 0x3);
+
+	tmp = UART_READ_REG(UART_LCR);
+	tmp |= (1<<7);
+	UART_WRITE_REG(UART_LCR, tmp);
+
+	UART_WRITE_REG(UART_THR, QUOT_VAL);
+	UART_WRITE_REG(UART_IER, 0x0);
+	UART_WRITE_REG(UART_SAMPLE_COUNT, SAMPLE_COUNT_VAL);
+	UART_WRITE_REG(UART_SAMPLE_POINT, SAMPLE_POINT_VAL);
+
+	tmp = UART_READ_REG(UART_LCR);
+	tmp &= ~(1<<7);
+	UART_WRITE_REG(UART_LCR, tmp);
 }
 
 static struct mm_region mt7622_mem_map[] = {

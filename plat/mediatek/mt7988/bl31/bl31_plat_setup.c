23a33769bbaa2aba09031192a2ed898be9b054d8
Weijie Gao <weijie.gao@mediatek.com>
2025-02-07 16:53:51 +0800

    mediatek: mt7988: add BL33/image decryption support
    Add support for decrypting BL33/image at runtime.
    
    Signed-off-by: Weijie Gao <weijie.gao@mediatek.com>

--- plat/mediatek/mt7988/bl31/bl31_plat_setup.c
+++ plat/mediatek/mt7988/bl31/bl31_plat_setup.c
@@ -16,6 +16,15 @@
 #include <emi_mpu.h>
 #include <devapc.h>
 
+#if ENCRYPT_BL33
+#include <bl33_dec.h>
+#include <common/debug.h>
+#endif
+
+#ifdef MTK_FW_ENC
+#include <fw_dec.h>
+#endif
+
 static void plat_efuse_init(void)
 {
 #if TRUSTED_BOARD_BOOT
@@ -35,6 +44,14 @@ void bl31_platform_setup(void)
 	infra_devapc_init();
 
 	plat_efuse_init();
+
+#if ENCRYPT_BL33
+	if (bl33_decrypt())
+		panic();
+#endif
+#ifdef MTK_FW_ENC
+	fw_dec_init();
+#endif
 }
 
 /*******************************************************************************

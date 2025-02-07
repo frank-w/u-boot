8681369a09aeb7753bbd0179270f39ff9b02d88f
Weijie Gao <weijie.gao@mediatek.com>
2025-02-07 16:45:49 +0800

    mediatek: mt7987: add build option dependency rules
    Add build option dependency rules to make sure option changes take
    effect on .o files immediately to avoid cleaning current build.
    
    Signed-off-by: Weijie Gao <weijie.gao@mediatek.com>

--- plat/mediatek/mt7987/platform.mk
+++ plat/mediatek/mt7987/platform.mk
@@ -53,3 +53,22 @@ include $(APSOC_COMMON)/img_dec/img_dec.mk
 include $(APSOC_COMMON)/bl2/tbbr_post.mk
 include $(APSOC_COMMON)/bl2/ar_post.mk
 include $(APSOC_COMMON)/bl2/bl2_image_post.mk
+
+# Make sure make command parameter reflects on .o files immediately
+include make_helpers/dep.mk
+
+$(call GEN_DEP_RULES,bl2,emicfg dram_log bl2_boot_ram bl2_boot_nand_nmbm bl2_dev_spi_nand bl2_dev_mmc bl2_plat_init bl2_plat_setup mt7987_gpio dtb)
+$(call MAKE_DEP,bl2,emicfg,DDR4_4BG_MODE DDR3_FREQ_2133 DDR3_FREQ_1866 DDR4_FREQ_3200 DDR4_FREQ_2666)
+$(call MAKE_DEP,bl2,dram_log,DRAM_DEBUG_LOG)
+$(call MAKE_DEP,bl2,bl2_plat_init,BL2_COMPRESS)
+$(call MAKE_DEP,bl2,bl2_plat_setup,BOOT_DEVICE TRUSTED_BOARD_BOOT DUAL_FIP BL32_TZRAM_BASE BL32_TZRAM_SIZE BL32_LOAD_OFFSET)
+$(call MAKE_DEP,bl2,bl2_dev_mmc,BOOT_DEVICE)
+$(call MAKE_DEP,bl2,bl2_dev_spi_nand,BOOT_DEVICE SPIM_NAND_NO_RETRY SPIM_NAND_PREFER_SPI2)
+$(call MAKE_DEP,bl2,bl2_boot_ram,RAM_BOOT_DEBUGGER_HOOK RAM_BOOT_UART_DL)
+$(call MAKE_DEP,bl2,bl2_boot_nand_nmbm,NMBM_MAX_RATIO NMBM_MAX_RESERVED_BLOCKS NMBM_DEFAULT_LOG_LEVEL)
+$(call MAKE_DEP,bl2,mt7987_gpio,ENABLE_JTAG)
+$(call MAKE_DEP,bl2,dtb,BOOT_DEVICE)
+
+$(call GEN_DEP_RULES,bl31,bl31_plat_setup plat_sip_calls)
+$(call MAKE_DEP,bl31,bl31_plat_setup,TRUSTED_BOARD_BOOT)
+$(call MAKE_DEP,bl31,plat_sip_calls,FWDL BL32_TZRAM_BASE BL32_TZRAM_SIZE BL32_LOAD_OFFSET)

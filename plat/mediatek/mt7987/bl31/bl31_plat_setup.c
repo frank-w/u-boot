fe5e2103b8b71dce6a333cdf7f23e6795d453c95
Weijie Gao <weijie.gao@mediatek.com>
2025-02-07 16:44:13 +0800

    mediatek: mt7987: add eFuse read/write support
    Add eFuse prebuilt library and relative SIP services for BL31 to allow
    BL33 to manipulate the eFuse features (SecureBoot, Anti-Rollback, ...)
    
    Signed-off-by: Weijie Gao <weijie.gao@mediatek.com>

--- plat/mediatek/mt7987/bl31/bl31_plat_setup.c
+++ plat/mediatek/mt7987/bl31/bl31_plat_setup.c
@@ -15,6 +15,7 @@
 #include <cpuxgpt.h>
 #include <emi_mpu.h>
 #include <devapc.h>
+#include <mtk_efuse.h>
 
 static void mt7987_disable_l2c_shared(void)
 {
@@ -59,6 +60,13 @@ static void platform_setup_sram(void)
 	}
 }
 
+static void plat_efuse_init(void)
+{
+#if TRUSTED_BOARD_BOOT
+	plat_efuse_sbc_init();
+#endif
+}
+
 /*******************************************************************************
  * Perform any BL3-1 platform setup code
  ******************************************************************************/
@@ -72,6 +80,8 @@ void bl31_platform_setup(void)
 
 	emi_mpu_init();
 	infra_devapc_init();
+
+	plat_efuse_init();
 }
 
 /*******************************************************************************
--- plat/mediatek/mt7987/platform.mk
+++ plat/mediatek/mt7987/platform.mk
@@ -44,6 +44,7 @@ include $(MTK_PLAT_SOC_BSP)/bl2pl/bl2pl.mk
 include $(MTK_PLAT_SOC_BSP)/bl2/bl2.mk
 include $(MTK_PLAT_SOC_BSP)/bl31/bl31.mk
 include $(MTK_PLAT_SOC_BSP)/bl32.mk
+include $(MTK_PLAT_SOC)/drivers/efuse/efuse.mk
 
 include $(APSOC_COMMON)/bl2/tbbr_post.mk
 include $(APSOC_COMMON)/bl2/bl2_image_post.mk

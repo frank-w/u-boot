dangowrt/arm-trusted-firmware

Makefile: add new bootloader stage for decompressing BL2 payload
This patch adds a new bootloader stage named BL2PL (aka. BL2 preloader)
to support BL2 compression.

This stage only implements XZ decompression for BL2 payload, and the BL2
payload must be compressed by XZ with crc32 checksum support.

To make use of this stage, a BL2PL specific image header must be added to
the compressed BL2 payload to record its load address and size. Then this
payload with header should be directly appended to the end of BL2PL raw
binary without any padding.

-------------------------------
| BL2PL | Header | XZ payload |
-------------------------------

On startup, BL2PL will first relocate itself to On-Chip SRAM and setup
the C environment, and then decompress the real BL2 to its load address,
and finally jump to the real BL2.

---------------------------------------------------------------
| Stage | On-Chip SRAM |            L2 shared SRAM            |
---------------------------------------------------------------
|   1   |              | BL2PL | XZ payload                   |
---------------------------------------------------------------
|   2   | BL2PL        |                           XZ payload |
---------------------------------------------------------------
|   3   | BL2PL        | BL2                     | XZ payload |
---------------------------------------------------------------

Also, add a tool named bl2plimage to generate BL2PL image header for XZ
compressed BL2 payload.

Signed-off-by: Weijie Gao <weijie.gao@mediatek.com>

bl2pl/bl2pl.mk
@@ -1,0 +1,13 @@
+#
+# Copyright (c) 2021 MediaTek Inc. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+include lib/xz/xz.mk
+
+BL2PL_SOURCES		+=	bl2pl/bl2pl_main.c			\
+				bl2pl/${ARCH}/bl2pl_entrypoint.S	\
+				$(XZ_SOURCES)
+
+BL2PL_LINKERFILE	:=	bl2pl/bl2pl.ld.S

frank-w/u-boot

bl2pl/bl2pl.mk
@@ -1,0 +1,13 @@
+#
+# Copyright (c) 2021 MediaTek Inc. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+include lib/xz/xz.mk
+
+BL2PL_SOURCES		+=	bl2pl/bl2pl_main.c			\
+				bl2pl/${ARCH}/bl2pl_entrypoint.S	\
+				$(XZ_SOURCES)
+
+BL2PL_DEFAULT_LINKER_SCRIPT_SOURCE	:=	bl2pl/bl2pl.ld.S
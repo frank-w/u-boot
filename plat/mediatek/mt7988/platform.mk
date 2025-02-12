mtk-openwrt/arm-trusted-firmware

mediatek: mt7988: add build option dependency rules
Add build option dependency rules to make sure option changes take effect
on .o files immediately to avoid cleaning current build.

Signed-off-by: Weijie Gao <weijie.gao@mediatek.com>

plat/mediatek/mt7988/platform.mk
@@ -55,3 +55,21 @@ OPTEE_TZRAM_SIZE := 0xfb0000
 endif
 endif
 CPPFLAGS += -DOPTEE_TZRAM_SIZE=$(OPTEE_TZRAM_SIZE)
+
+# Make sure make command parameter reflects on .o files immediately
+include make_helpers/dep.mk
+
+$(call GEN_DEP_RULES,bl2,emicfg bl2_boot_ram bl2_boot_nand_nmbm bl2_dev_mmc mtk_efuse bl2_plat_init bl2_plat_setup mt7988_gpio pll)
+$(call MAKE_DEP,bl2,emicfg,DRAM_USE_COMB DRAM_USE_DDR4 DRAM_SIZE_LIMIT DRAM_DEBUG_LOG)
+$(call MAKE_DEP,bl2,bl2_plat_init,BL2_COMPRESS I2C_SUPPORT EIP197_SUPPORT BL2_CPU_FULL_SPEED)
+$(call MAKE_DEP,bl2,bl2_plat_setup,BOOT_DEVICE TRUSTED_BOARD_BOOT)
+$(call MAKE_DEP,bl2,bl2_dev_mmc,BOOT_DEVICE)
+$(call MAKE_DEP,bl2,bl2_boot_ram,RAM_BOOT_DEBUGGER_HOOK RAM_BOOT_UART_DL)
+$(call MAKE_DEP,bl2,bl2_boot_nand_nmbm,NMBM_MAX_RATIO NMBM_MAX_RESERVED_BLOCKS NMBM_DEFAULT_LOG_LEVEL)
+$(call MAKE_DEP,bl2,mtk_efuse,ANTI_ROLLBACK TRUSTED_BOARD_BOOT)
+$(call MAKE_DEP,bl2,mt7988_gpio,ENABLE_JTAG)
+$(call MAKE_DEP,bl2,pll,BL2_CPU_FULL_SPEED)
+
+$(call GEN_DEP_RULES,bl31,mtk_efuse plat_sip_calls)
+$(call MAKE_DEP,bl31,mtk_efuse,ANTI_ROLLBACK TRUSTED_BOARD_BOOT)
+$(call MAKE_DEP,bl31,plat_sip_calls,FWDL)

frank-w/u-boot

plat/mediatek/mt7988/platform.mk
@@ -51,3 +51,21 @@ OPTEE_TZRAM_SIZE := 0x500000
 endif
 endif
 CPPFLAGS += -DOPTEE_TZRAM_SIZE=$(OPTEE_TZRAM_SIZE)
+
+# Make sure make command parameter reflects on .o files immediately
+include make_helpers/dep.mk
+
+$(call GEN_DEP_RULES,bl2,emicfg bl2_boot_ram bl2_boot_nand_nmbm bl2_dev_mmc bl2_plat_init bl2_plat_setup mt7988_gpio pll dtb)
+$(call MAKE_DEP,bl2,emicfg,DRAM_USE_COMB DRAM_USE_DDR4 DRAM_SIZE_LIMIT DRAM_DEBUG_LOG)
+$(call MAKE_DEP,bl2,bl2_plat_init,BL2_COMPRESS I2C_SUPPORT EIP197_SUPPORT BL2_CPU_FULL_SPEED)
+$(call MAKE_DEP,bl2,bl2_plat_setup,BOOT_DEVICE TRUSTED_BOARD_BOOT)
+$(call MAKE_DEP,bl2,bl2_dev_mmc,BOOT_DEVICE)
+$(call MAKE_DEP,bl2,bl2_boot_ram,RAM_BOOT_DEBUGGER_HOOK RAM_BOOT_UART_DL)
+$(call MAKE_DEP,bl2,bl2_boot_nand_nmbm,NMBM_MAX_RATIO NMBM_MAX_RESERVED_BLOCKS NMBM_DEFAULT_LOG_LEVEL)
+$(call MAKE_DEP,bl2,mt7988_gpio,ENABLE_JTAG)
+$(call MAKE_DEP,bl2,pll,BL2_CPU_FULL_SPEED)
+$(call MAKE_DEP,bl2,dtb,BOOT_DEVICE)
+
+$(call GEN_DEP_RULES,bl31,bl31_plat_setup plat_sip_calls)
+$(call MAKE_DEP,bl31,bl31_plat_setup,ANTI_ROLLBACK TRUSTED_BOARD_BOOT)
+$(call MAKE_DEP,bl31,plat_sip_calls,FWDL)
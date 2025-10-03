95f1a063fca253256d1d887069c675a47382182c
bruce cheung <bruce.cheung@emplustech.com>
2025-10-03 20:20:30 +0800

    [bpi-r3] Add run.sh to support building model bpi-r3

---
+++ run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+function usage {
+    echo "Usage: $0 <model>"
+    echo "Available models: bpi-r3"
+}
+
+[ $# -ne 1 ] && usage && exit 1
+
+model=$1
+
+case $model in
+    bpi-r3) ;;
+    *) echo "Unsupported model: $model" && usage && exit 1 ;;
+esac
+
+time {
+    sudo losetup -D
+    make clean
+    echo -e "board=${model}\ndevice=emmc" > build.conf
+    ./build.sh importconfig
+    ./build.sh
+    ./build.sh rename
+    git checkout -f mtk-atf-2025
+    echo -e "board=${model}\ndevice=emmc" > build.conf
+    ./build.sh
+    ./build.sh rename
+    ./build.sh createimg non-interactive
+    rm build.conf
+    git checkout -f 2025-07-bpi
+}

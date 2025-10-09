b11d1831e6c21ac91b569d4cee18c264621d37d8
bruce cheung <bruce.cheung@emplustech.com>
2025-10-09 19:51:12 +0800

    [bpi-r4] Patch run.sh to support building model bpi-r4

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 function usage {
     echo "Usage: $0 <model>"
-    echo "Available models: bpi-r3"
+    echo "Available models: bpi-r3 bpi-r4"
 }
 
 [ $# -ne 1 ] && usage && exit 1
@@ -11,6 +11,7 @@ model=$1
 
 case $model in
     bpi-r3) ;;
+    bpi-r4) ;;
     *) echo "Unsupported model: $model" && usage && exit 1 ;;
 esac
 
@@ -26,6 +27,12 @@ time {
     ./build.sh
     ./build.sh rename
     ./build.sh createimg non-interactive
+    if [ $model = "bpi-r4" ]; then
+        make realclean
+        echo -e "extraflags=DDR4_4BG_MODE=1" >> build.conf
+        ./build.sh
+        ./build.sh rename
+    fi
     rm build.conf
     git checkout -f 2025-07-bpi
 }
